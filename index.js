e23f575a31f35ae7a279fa10e8772baff311870c
unknown <ulisacosta@gmail.com>
2023-03-18 10:22:08 -0300

    agrego funcionalidad de cuando cambia la pagina cambia el titulo

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let previousTitle = document.title;
+
+
+window.addEventListener('blur', ()=>{
+    previousTitle = document.title;
+    document.title = "Protegemos lo que tanto te costo tener"
+
+})
+
+window.addEventListener('focus',()=>{
+    document.title = previousTitle
+})
