5e9af366ec1984a06eda374def6b37c6e0f25500
ulises <ulisacost@gmail.com>
2023-08-04 19:59:45 -0300

    agrego mejoras para el slider

--- js/sliders.js
+++ js/sliders.js
@@ -1,4 +1,4 @@
-const splide = document.querySelector('.splide')
+const splide = document.querySelector(".splide");
 
 /* const splideCompañias = new Splide(splide, {
     type   : 'loop',
@@ -7,17 +7,20 @@ const splide = document.querySelector('.splide')
    
 }); */
 
-
-    new Splide( '#image-carousel',{
-        type   : 'loop',
-        drag   : 'free',
-        focus  : 'center',
-        perPage: 1,
-        autoScroll: {
-          speed: 1,
-        },
- 
-     
-    }).mount();
+new Splide("#image-carousel", {
+  type    : 'loop',
+  autoplay: 'play',
+  perPage : 1,
+  gap:5,
+  arrows: false,
+  autoHeight: true,
+  autoWidth: true,
+  autoplay: true,
+  pauseOnHover: true,
+  pagination: false,
+  rewind: true,
+  speed:1500,
+ interval:1000
+}).mount();
 
 /* splideCompañias.mount( ) */
